4a347c3c1268ed91b378dff7a2b080680b10df65
Jonathan Robles <jonathan.robles287@gmail.com>
2022-04-22 23:26:46 -0500

    Created Custom User Details Service

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,5 +1,8 @@
 import com.grailsLearning.UserPasswordEncoderListener
+import com.grailsLearning.CustomUserDetailsService
 // Place your Spring DSL code here
+
 beans = {
     userPasswordEncoderListener(UserPasswordEncoderListener)
+    userDetailsService(CustomUserDetailsService)
 }
